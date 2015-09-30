f7b5e76c05f6f60193c5a02a4929e5e88f4103ae
flada-auxv <aseknock@gmail.com>
2015-10-01 00:20:41 +0900

    Hello world!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate iron;
+
+use iron::prelude::*;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Hello world!!")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(hello_world).http("localhost:3000").unwrap();
 }
