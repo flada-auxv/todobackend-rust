042d1316e90f56759fd284ab37a1e078568366ca
flada-auxv <aseknock@gmail.com>
2015-10-03 15:32:35 +0900

    Implement 'the api root responds to a GET(CORS headers are set up, too)'

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 extern crate iron;
+extern crate router;
+extern crate unicase;
 
 use iron::prelude::*;
+use iron::status;
+use iron::headers;
+use iron::method::{Options, Get, Post};
+use iron::AfterMiddleware;
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((iron::status::Ok, "Hello world!!")))
+use unicase::UniCase;
+
+struct CorsSupport;
+
+impl AfterMiddleware for CorsSupport {
+    fn after(&self, _: &mut Request, mut res: Response) -> IronResult<Response> {
+        res.headers.set(headers::AccessControlAllowOrigin::Any);
+        res.headers.set(headers::AccessControlAllowHeaders(vec![UniCase("content-type".to_owned()), UniCase("accept".to_owned())]));
+        res.headers.set(headers::AccessControlAllowMethods(vec![Options, Get, Post]));
+        Ok(res)
+    }
 }
 
 fn main() {
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    let mut router = router::Router::new();
+    router.get("/", |_: &mut Request| {
+        let res = Response::with((status::Ok, "Hello World!"));
+        Ok(res)
+    });
+
+    let mut chain = Chain::new(router);
+    chain.link_after(CorsSupport);
+
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
